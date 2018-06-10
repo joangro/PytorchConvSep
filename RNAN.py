e44d4402d892d05ea755f1a967e3487724105923
jgraunoel@gmail.com <jgraunoel@gmail.com>
2018-06-10 04:33:51 +0200

    fixed training function

--- RNAN.py
+++ RNAN.py
@@ -10,6 +10,7 @@ import time
 from PytorchConvSep import AutoEncoder#, loss_calc
 from data_pipeline import data_gen
 import config
+import utils
 
 class Encoder(nn.Module):
     def __init__(self):
@@ -19,7 +20,7 @@ class Encoder(nn.Module):
             nn.Conv2d(2, 2, (1, 386), stride = 1, padding = 0, bias = True),
         )
         
-        self.rnn = nn.LSTM(256, 256,2, batch_first= True)
+        self.rnn = nn.LSTM(64, 64,2, batch_first= True)
         
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(2, 2, (1, 386), stride = 1, padding = 0, bias = True),
@@ -30,10 +31,9 @@ class Encoder(nn.Module):
 
         encode = self.encoder(x)
         encode = torch.squeeze(encode.view(4,30, -1))
-
         lstm,_ = self.rnn(encode)
         
-        to_decode = lstm.contiguous().view(4, 2, 30, 128)
+        to_decode = lstm.contiguous().view(1, 2, 30, 128)
 
         decoded = self.decoder(to_decode)
         return decoded
@@ -48,7 +48,7 @@ def GenerateRandomData(seed = 2451, dimension = [30, 513]):
     '''
     return np.random.rand(4, 2, 30, 513)
     
-def trainNetwork(dataset='model1'):
+def trainNetwork(dataset='model6'):
     save_name = 'dn_model'
     # Encoder
     denoiser_vocals = Encoder().cuda()
@@ -56,7 +56,7 @@ def trainNetwork(dataset='model1'):
     
     autoencoder.load_state_dict(torch.load(config.log_dir + dataset + '.pt'))
     
-    optimizer   =  torch.optim.Adagrad(denoiser_vocals.parameters(), 0.0001 )
+    optimizer   =  torch.optim.Adam(denoiser_vocals.parameters(), 0.00001 )
 
     loss_func   =  nn.MSELoss( size_average=False )
 
@@ -64,9 +64,8 @@ def trainNetwork(dataset='model1'):
     
     train_evol = []
 
-    val_evol = []
+    eval_evol = []
     
-    count = 0
     
     for epoch in range(config.dn_num_epochs):
     
@@ -77,10 +76,12 @@ def trainNetwork(dataset='model1'):
         val_gen = data_gen(mode= "Val")
         
         optimizer.zero_grad()
-        
+        train_loss = 0
+        eval_loss = 0
+        count = 0
         for inputs, targets in train_gen:
             
-            out_sources = autoencoder(inputs)
+            output = autoencoder(Variable(torch.FloatTensor(inputs))).cuda()
             
             vocals = output[:,:2,:,:]
 
@@ -100,20 +101,21 @@ def trainNetwork(dataset='model1'):
 
             mask_others = 1 - (mask_vocals+mask_drums+mask_bass)
 
-            out_vocals = in_batches * mask_vocals
+            out_vocals = vocals * mask_vocals
 
-            out_drums = in_batches * mask_drums
+            out_drums = drums * mask_drums
 
-            out_bass = in_batches * mask_bass
+            out_bass = bass * mask_bass
 
-            out_others = in_batches * mask_others
+            out_others = others * mask_others
+
+            input_vocals = Variable(out_vocals)
+           
+            denoised_vocals = denoiser_vocals(input_vocals).cuda()
             
-            denoised_vocals = denoiser_vocals(Variable(torch.FloatTensor(out_vocals)).cuda())
-            
-            step_loss = loss_func(denoised_vocals, targets[:,:2,:,:])
+            step_loss = loss_func(denoised_vocals, Variable(vocals, requires_grad = False))
             
             train_loss += step_loss.item()
-            
             step_loss.backward()
             
             optimizer.step()
@@ -121,12 +123,12 @@ def trainNetwork(dataset='model1'):
             utils.progress(count,config.batches_per_epoch_train, suffix = 'training done')
 
             count+=1
-            
+        train_evol.append(train_loss)   
         count = 0
 
         for inputs, targets in val_gen:
         
-            out_sources = autoencoder(inputs)
+            out_sources = autoencoder(Variable(torch.FloatTensor(inputs))).cuda()
             
             vocals = output[:,:2,:,:]
 
@@ -146,30 +148,32 @@ def trainNetwork(dataset='model1'):
 
             mask_others = 1 - (mask_vocals+mask_drums+mask_bass)
 
-            out_vocals = in_batches * mask_vocals
+            out_vocals = vocals * mask_vocals
 
-            out_drums = in_batches * mask_drums
+            out_drums = drums * mask_drums
 
-            out_bass = in_batches * mask_bass
+            out_bass = bass * mask_bass
 
-            out_others = in_batches * mask_others
-            
-            denoised_vocals = denoiser_vocals(Variable(torch.FloatTensor(out_vocals)).cuda())
+            out_others = others * mask_others
+            input_vocals = Variable(out_vocals)
+            denoised_vocals = denoiser_vocals(input_vocals).cuda()
         
-            step_loss = loss_func(denoised_vocals, targets[:,:2,:,:])
+            step_loss = loss_func(denoised_vocals, Variable(vocals, requires_grad = False))
             
-            val_evol += step_loss.item()
-            
+            eval_loss += step_loss.item()
+            utils.progress(count,config.batches_per_epoch_val, suffix = 'validation done')
+            count += 1
+        eval_evol.append(eval_loss)    
         duration = time.time()-start_time
 
         if (epoch+1)%config.print_every == 0:
             print('epoch %d/%d, took %.2f seconds, epoch total loss: %.7f' % (epoch+1, config.num_epochs, duration, train_loss))
-            print('                                  validation total loss: %.7f' % ( val_loss))
+            print('                                  validation total loss: %.7f' % ( eval_loss))
             
         if (epoch+1)%config.save_every  == 0:
-            torch.save(autoencoder.state_dict(), config.log_dir+save_name+'_'+str(epoch)+'.pt')
+            torch.save(autoencoder.state_dict(), config.log_dir_dn+save_name+'_'+str(epoch)+'.pt')
             np.save(config.log_dir+'dn_train_loss',np.array(train_evol))
-            np.save(config.log_dir+'dn_val_loss',np.array(val_evol))
+            np.save(config.log_dir+'dn_val_loss',np.array(eval_evol))
             
             
 if __name__ == "__main__":
