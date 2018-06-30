Fixed algorithm: finally gives sufficiently good results Fixed data pipeline: added algorithm to avoid giving out silent mixture chunks

PytorchConvSep.py
@@ -17,12 +17,12 @@ import stempeg
 
 def loss_calc(inputs, targets, loss_func, autoencoder):
 
-    eps=1e-18
+    eps=1e-9
+    #import pdb;pdb.set_trace()
+    targets = targets *np.linspace(1.0,0.5,513)
 
-    targets = targets *np.linspace(1.0,0.7,513)
-
-    targets_cuda = Variable(torch.FloatTensor(targets)).cuda()
-    inputs = Variable(torch.FloatTensor(inputs)).cuda()
+    targets_cuda = Variable(torch.FloatTensor(targets)).cuda() + eps
+    inputs = Variable(torch.FloatTensor(inputs)).cuda() + eps
 
 
     output = autoencoder(inputs) + eps
@@ -81,7 +81,7 @@ def loss_calc(inputs, targets, loss_func, autoencoder):
     return step_loss_vocals, step_loss_drums, step_loss_bass, alpha_diff, beta_other, beta_other_voc
 
 class AutoEncoder(nn.Module):
-    def __init__(self, conv_hor_in = (1, 513), conv_ver_in = (12, 1)):
+    def __init__(self, conv_hor_in = (1, 513), conv_ver_in = (15, 1)):
         '''
         I tried to make this as customizable as possible.
         INPUT:
@@ -108,100 +108,88 @@ class AutoEncoder(nn.Module):
         # we need to use sequential, as it's a way to add modules one after the 
         # another in an ordered way
         self.encoder = nn.Sequential(
-            nn.Conv2d(2, 2, self.conv_hor, stride = 1, padding = 0, bias = True),
-            nn.Conv2d(2, 2, self.conv_ver, stride = 1, padding = 0, bias = True)
+            nn.Conv2d(2, config.num_ch_out_hor, self.conv_hor, stride = 1, padding = 0, bias = True, groups = 1),
+            nn.Conv2d(config.num_ch_out_hor, config.num_ch_out_ver, self.conv_ver, stride = 1, padding = 0, bias = True, groups = 1)
         )
         
         ### DECODERS
         self.decode_drums = nn.Sequential(
-            nn.ConvTranspose2d(2, 2, self.conv_ver, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_ver, config.num_ch_out_hor, self.conv_ver, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU(),
-            nn.ConvTranspose2d(2, 2, self.conv_hor, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_hor, 2, self.conv_hor, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU()
         )
         self.decode_voice = nn.Sequential(
-            nn.ConvTranspose2d(2, 2, self.conv_ver, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_ver, config.num_ch_out_hor, self.conv_ver, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU(),
-            nn.ConvTranspose2d(2, 2, self.conv_hor, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_hor, 2, self.conv_hor, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU()
         )
         self.decode_bass = nn.Sequential(
-            nn.ConvTranspose2d(2, 2, self.conv_ver, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_ver, config.num_ch_out_hor, self.conv_ver, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU(),
-            nn.ConvTranspose2d(2, 2, self.conv_hor, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_hor, 2, self.conv_hor, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU()
         )
         self.decode_other = nn.Sequential(
-            nn.ConvTranspose2d(2, 2, self.conv_ver, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_ver, config.num_ch_out_hor, self.conv_ver, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU(),
-            nn.ConvTranspose2d(2, 2, self.conv_hor, stride = 1, padding = 0, bias = True),
+            nn.ConvTranspose2d(config.num_ch_out_hor, 2, self.conv_hor, stride = 1, padding = 0, bias = True, groups = 1),
             # nn.ReLU()
         )
         
         ### FULLY CONNECTED LAYERS
-        self.layer_first = nn.Linear(38, 128)
+        self.layer_first = nn.Sequential(
+            nn.Linear(config.num_ch_out_ver*16, 128),
+            nn.ReLU()
+        )
 
         self.layer_drums = nn.Sequential(
-            nn.Linear(128, 38),
-            # nn.ReLU()
+            nn.Linear(128,config.num_ch_out_ver*16),
+            nn.ReLU()
         )
-        self.layer_voice = nn.Sequential(
-            nn.Linear(128, 38),
-            # nn.ReLU()
-        )
-        self.layer_bass = nn.Sequential(
-            nn.Linear(128, 38),
-            # nn.ReLU()
-        )
-        self.layer_other = nn.Sequential(
-            nn.Linear(128, 38),
-            # nn.ReLU()
-        )
-        
-        # put the layers and deconv in libraries to make it easier to work with
-        self.layers = OrderedDict([
-            ("voice", self.layer_voice),
-            ("drums", self.layer_drums),
-            ("bass",  self.layer_bass),
-            ("other", self.layer_other)
-            ])
-            
-        self.deconvs = OrderedDict([
-            ("voice", self.decode_voice),
-            ("drums", self.decode_drums),
-            ("bass",  self.decode_bass),
-            ("other", self.decode_other)
-            ])
-                
-        # OUTPUT MATRIX
-        # Create a tensor variable with shape (15, 1, 30, 513)
-        # care, as the channels dimension is initialized with 1, and we are appending to it
-        self.final_output = Variable()
 
+        self.layer_voice = nn.Sequential(
+            nn.Linear(128,config.num_ch_out_ver*16),
+            nn.ReLU()
+        )
+
+        self.layer_bass = nn.Sequential(
+            nn.Linear(128,config.num_ch_out_ver*16),
+            nn.ReLU()
+        )
+        self.layer_other =  nn.Sequential(
+            nn.Linear(128,config.num_ch_out_ver*16),
+            nn.ReLU()
+        )
         
+        self.final_output = nn.ReLU()
+
     def forward(self, x):  
           
         encode = self.encoder(x)
-
         encode = encode.view(config.batch_size, -1)
-
         layer_output = self.layer_first(encode)
-        
-        output_flag = 0
-        
-        for key in self.layers:
-            
-            source_output = self.layers[key](layer_output)
+        layer_output_voice = self.layer_voice(layer_output) 
+        layer_output_voice = layer_output_voice.view(-1,config.num_ch_out_ver,16,1)
+        output_voice = self.decode_voice(layer_output_voice)
 
-            source_deconv = self.deconvs[key](source_output.view(-1,2,19,1))
+        layer_output_drums = self.layer_drums(layer_output)
+        layer_output_drums = layer_output_drums.view(-1,config.num_ch_out_ver,16,1)
+        output_drums = self.decode_drums(layer_output_drums)
 
-            if  output_flag == 0:
-                self.final_output = source_deconv
-                output_flag = 1
-            else:
-                self.final_output = torch.cat((source_deconv, self.final_output), dim = 1)
-                
-        return self.final_output
+        layer_output_bass = self.layer_bass(layer_output)
+        layer_output_bass = layer_output_bass.view(-1,config.num_ch_out_ver,16,1)
+        output_bass = self.decode_bass(layer_output_bass)
+
+        layer_output_other = self.layer_other(layer_output)
+        layer_output_other = layer_output_other.view(-1,config.num_ch_out_ver,16,1)
+        output_other = self.decode_other(layer_output_other)
+        
+        reshape_output = torch.cat((output_voice, output_drums, output_bass, output_other), 1)
+        output_final = self.final_output(reshape_output)
+
+        return output_final     
 
 
 
@@ -213,10 +201,12 @@ def trainNetwork(save_name = 'model_e' + str(config.num_epochs) + '_b' + str(con
 
     autoencoder =  AutoEncoder().cuda()
 
-    optimizer   =  torch.optim.Adam(autoencoder.parameters(), lr = 0.000001)
+    autoencoder.load_state_dict(torch.load('./log/model_e8000_b50_bs5_519.pt'))
 
-    #loss_func   =  nn.MSELoss( size_average=False )
-    loss_func   =  nn.L1Loss( size_average=False )
+    optimizer   =  torch.optim.Adadelta(autoencoder.parameters(), lr = 1, rho=0.95)
+
+    loss_func   =  nn.MSELoss( size_average=False )
+    #loss_func   =  nn.L1Loss( size_average=False )
 
     train_evol = []
 
@@ -268,7 +258,8 @@ def trainNetwork(save_name = 'model_e' + str(config.num_epochs) + '_b' + str(con
 
             train_loss += step_loss.item()
             if np.isnan(train_loss):
-               # import pdb;pdb.set_trace()
+               #import pdb;pdb.set_trace()
+               optimizer.zero_grad()
                print ("error output contains NaN")
             train_loss_vocals +=step_loss_vocals.item()
             train_loss_drums +=step_loss_drums.item()
@@ -278,8 +269,16 @@ def trainNetwork(save_name = 'model_e' + str(config.num_epochs) + '_b' + str(con
             train_beta_other_voc+=beta_other_voc.item()          
 
             step_loss.backward()
-
+            #clip gradient
+            # torch.nn.utils.clip_grad_norm_( autoencoder.parameters(),1)
+            
+            for p in autoencoder.parameters():
+                p.grad.data.clamp(-1,1)
+                  
+            
             optimizer.step()
+
+
             # print time.time()-start_time
 
             utils.progress(count,config.batches_per_epoch_train, suffix = 'training done')
@@ -348,24 +347,26 @@ def trainNetwork(save_name = 'model_e' + str(config.num_epochs) + '_b' + str(con
 
         # import pdb;pdb.set_trace()
         if (epoch+1)%config.save_every  == 0:
-            torch.save(autoencoder.state_dict(), config.log_dir+save_name+'_'+str(epoch)+'.pt')
+            torch.save(autoencoder.state_dict(), config.log_dir+save_name+'_'+str(epoch + 519)+'.pt')
             np.save(config.log_dir+'train_loss',np.array(train_evol))
             np.save(config.log_dir+'val_loss',np.array(val_evol))
         # import pdb;pdb.set_trace()
 
 
-    torch.save(autoencoder.state_dict(), config.log_dir+save_name+'_'+str(epoch)+'.pt')
+    torch.save(autoencoder.state_dict(), config.log_dir+save_name+'_'+str(epoch + 99)+'.pt')
 
 
-def evalNetwork(file_name, load_name='model_e4000_b500_bs1_1699', plot = False, synth = False):
+def evalNetwork(file_name, load_name='model_e4000_b50_bs5_1709', plot = False, synth = False):
     autoencoder_audio = AutoEncoder().cuda()
     epoch = 50
-    # autoencoder_audio.load_state_dict(torch.load(config.log_dir+load_name+'_'+str(epoch)+'.pt'))
-    autoencoder_audio.load_state_dict(torch.load(config.log_dir+'model1/'+load_name+'.pt'))
 
+    eps=1e-11
+
+    # autoencoder_audio.load_state_dict(torch.load(config.log_dir+load_name+'_'+str(epoch)+'.pt'))
+    autoencoder_audio.load_state_dict(torch.load('./log/model_e8000_b50_bs5_519.pt'))
     stat_file = h5py.File(config.stat_dir+'stats.hdf5', mode='r')
-    # import pdb;pdb.set_trace()
-    max_feat = np.array(stat_file["feats_maximus"])
+    # import pdb;pdb.set_trace():q!
+    max_feat = np.array(stat_file["feats_maximus"])   
     min_feat = np.array(stat_file["feats_minimus"])
 
     max_feat_tars = max_feat[:8,:].reshape(8,1,513)
@@ -405,8 +406,8 @@ def evalNetwork(file_name, load_name='model_e4000_b500_bs1_1699', plot = False, 
 
     for in_batch in in_batches:
         # import pdb;pdb.set_trace()
-        in_batch = Variable(torch.FloatTensor(in_batch)).cuda()
-        out_batch = autoencoder_audio(in_batch)
+        in_batch = Variable(torch.FloatTensor(in_batch)).cuda() + eps
+        out_batch = autoencoder_audio(in_batch) + eps
         out_batches.append(np.array(out_batch.data.cpu().numpy()))
         
 
@@ -422,7 +423,9 @@ def evalNetwork(file_name, load_name='model_e4000_b500_bs1_1699', plot = False, 
 
     others = out_batches[:,:,6:,:,:]
 
-    total_sources = vocals + bass + drums + others
+    total_sources = vocals + bass + drums + others 
+
+    total_sources = total_sources
 
     mask_vocals = vocals/total_sources
 

data_pipeline.py
@@ -5,17 +5,18 @@ import h5py
 
 import config
 
-def data_gen(mode = 'Train'):
+def data_gen(mode = 'Train', data_aug = False):
     stat_file = h5py.File(config.stat_dir+'stats.hdf5', mode='r')
     #import pdb;pdb.set_trace()
     max_feat = np.array(stat_file["feats_maximus"])
     min_feat = np.array(stat_file["feats_minimus"])
-
+    
     max_feat_tars = max_feat[:8,:].reshape(1,8,1,513)
     min_feat_tars = min_feat[:8,:].reshape(1,8,1,513)
 
     max_feat_ins = max_feat[-2:,:].reshape(1,2,1,513)
     min_feat_ins = min_feat[-2:,:].reshape(1,2,1,513)
+    
     if mode == "Train":
         in_dir=config.dir_hdf5
         num_batches = config.batches_per_epoch_train
@@ -23,7 +24,7 @@ def data_gen(mode = 'Train'):
         in_dir = config.dir_hdf5_test
         num_batches = config.batches_per_epoch_val
 
-    sources = ['voc_stft', 'drums_stft', 'bass_stft', 'acc_stft']
+    sources = range(4)
     
     file_list = [x for x in os.listdir(in_dir) if x.endswith('.hdf5') and not x.startswith('._')]
 
@@ -40,132 +41,76 @@ def data_gen(mode = 'Train'):
         #start_time = time.time()
 
         for i in range(max_files_to_process):
+        
+            if data_aug is True:
+                #p = np.random.random_sample()
+                p = 0.1
+                print (p)
+                if p < 0.4:
+                    mix_stft = []
+                    print ("IEE")
+                    for source in sources:
+                        print (source)
+                        file_index = np.random.randint(0,num_files)
+                        file_to_open = file_list[file_index]
 
-            file_index = np.random.randint(0,num_files)
-            
-            file_to_open = file_list[file_index]
+                        hdf5_file = h5py.File(in_dir+file_to_open, "r")
 
-            hdf5_file = h5py.File(in_dir+file_to_open, "r")
-
-            tar_stft = hdf5_file["tar_stft"]
-
-            mix_stft = hdf5_file['mix_stft']
-
-
-
-            file_len = mix_stft.shape[1]
-            # start_time = time.time()
-            for j in range(config.samples_per_file):
-                index=np.random.randint(0,file_len-config.max_phr_len)
-                # targets.append(np.concatenate((voc_stft[:,index:index+config.max_phr_len,:],drums_stft[:,index:index+config.max_phr_len,:],bass_stft[:,index:index+config.max_phr_len,:],acc_stft[:,index:index+config.max_phr_len,:]),axis=0))
-
-                targets.append(tar_stft[:,index:index+config.max_phr_len,:])
-                inputs.append(mix_stft[:,index:index+config.max_phr_len,:])
-            hdf5_file.close()
-
-            # print("One file took %0.00f" % (time.time()-start_time))
-        #import pdb;pdb.set_trace()
-        targets = (np.array(targets)-min_feat_tars)/(max_feat_tars-min_feat_tars)
-        inputs = (np.array(inputs)-min_feat_ins)/(max_feat_ins-min_feat_ins)
-        yield inputs, targets
-            
-            # p = np.random.random_sample()
-            
-            # # Randomize which batches are augmentated
-            # if config.data_aug is True and p < -1:
-            #     #print ('random')
-            #     # each sample is a different file
-            #     for j in range(config.samples_per_file):
-                    
-            #         # Random file for each source
-            #         file_index = [np.random.randint(0,num_files) for x in range(4)]
-                    
-            #         source_i = 0
-                    
-            #         mix_stft = []
-            #         mix_stft = np.ndarray(mix_stft)
-                    
-            #         sources_stft = []
-                    
-            #         for source in sources:
-            #             file_to_open = file_list[file_index[source_i]]
+                        source_stft = hdf5_file["tar_stft"]
                         
-            #             hdf5_file = h5py.File(in_dir+file_to_open, "r")
-                        
-            #             source_stft = hdf5_file[source]
-            #             file_len = source_stft.shape[1]
-                        
-            #             # random stft time index
-            #             index=np.random.randint(0,file_len-config.max_phr_len)
-                        
-            #             source_stft = source_stft[:,index:index+config.max_phr_len,:]
-                        
-            #             if source_i == 0:
-            #                 sources_stft = source_stft
+                        file_len = source_stft.shape[1]
+                                                
+                        source_stft_c = source_stft[source*2:source*2+2,:,:]
+                        for j in range(config.samples_per_file):
+                            index=np.random.randint(0,file_len-config.max_phr_len)
+                            source_stft_f = source_stft_c[:,index:index+config.max_phr_len,:]
                             
-            #                 # this might be wrong, but I think it still makes sense:
-            #                 mix_stft = source_stft/4
-                            
-            #             else:
-            #                 sources_stft = np.concatenate((sources_stft, source_stft),axis=0)
-                            
-            #                 mix_stft += source_stft/4
-                            
-            #             source_i += 1
+                        if source is 0:
+                            mix_stft = source_stft_f
+                            targets_all = source_stft_f
+                        else:
+                            mix_stft += source_stft_f
+                            targets_all =np.concatenate((targets_all,source_stft_f), axis = 0)
                         
-            #         targets.append(sources_stft)
-            #         inputs.append(mix_stft)
+                    targets.append(targets_all)
+                    inputs.append(mix_stft)
+                #yield inputs, targets
 
-            # else:
-
+                    
+            else:
+                file_index = np.random.randint(0,num_files)
                 
-                # Normalize data 
-        #         for j in range(config.samples_per_file):
-        #             index=np.random.randint(0,file_len-config.max_phr_len)
-        #             stfts = [ voc_stft[:,index:index+config.max_phr_len,:],\
-        #                       drums_stft[:,index:index+config.max_phr_len,:],\
-        #                       bass_stft[:,index:index+config.max_phr_len,:],\
-        #                       acc_stft[:,index:index+config.max_phr_len,:],\
-        #                       mix_stft[:,index:index+config.max_phr_len,:]]   
-        #             source_index = 0
-                    
-        #             for source_stft in stfts:
-        #                 stft_max = np.amax(source_stft[:,:,:], axis = 1)
-                        
-        #                 if np.amax(stft_max) is not 0:
-        #                     stft_max[stft_max == 0] = 0.0001 # for the indexes where the maximum is still zero
-        #                     stft_norm = [source_stft[:,x,:] / stft_max for x in range(config.max_phr_len)]
-        #                     stft_norm = np.array(stft_norm).view().reshape((2, 30, 513))
-        #                 else:
-        #                     # in the case where all the stft is made of zeros
-        #                     stft_norm = source_stft
-                        
-        #                 #print(np.array(stft_norm).shape)
-        #                 #print (np.amax(np.array(stft_norm)))
-        #                 if source_index < 4:
-        #                     if source_index == 0:
-        #                         stft_stream = np.array(source_stft)
-        #                     else:
-        #                         stft_stream = np.concatenate((stft_stream, stft_norm), axis = 0)
-        #                 else:
-        #                     inputs.append(stft_norm)
-        #                 source_index += 1
-        #             targets.append(stft_stream)
-                
-        # #print(time.time()-start_time)
-        # # normalization
-        # '''
-        # max_inp = np.amax(inputs,axis = 0)
-        # print (max_inp.shape)
-        # '''
-        # # print (np.array(targets).shape)
-        # #print (np.array(inputs).shape)
-        # yield np.array(inputs), np.array(targets)
+                file_to_open = file_list[file_index]
+
+                hdf5_file = h5py.File(in_dir+file_to_open, "r")
+
+                tar_stft = hdf5_file["tar_stft"]
+
+                mix_stft = hdf5_file['mix_stft']
 
 
-    
-    #import pdb;pdb.set_trace()
 
+                file_len = mix_stft.shape[1]
+                # start_time = time.time()
+                for j in range(config.samples_per_file):
+                    flag = False
+                    while flag is False:
+                        index=np.random.randint(0,file_len-config.max_phr_len)#;print ('small')
+                    # targets.append(np.concatenate((voc_stft[:,index:index+config.max_phr_len,:],drums_stft[:,index:index+config.max_phr_len,:],bass_stft[:,index:index+config.max_phr_len,:],acc_stft[:,index:index+config.max_phr_len,:]),axis=0))
+                        #import pdb;pdb.set_trace()
+                        if mix_stft[:,index:index+config.max_phr_len,:425].mean() > 0.02:
+                            targets.append(tar_stft[:,index:index+config.max_phr_len,:])
+                            inputs.append(mix_stft[:,index:index+config.max_phr_len,:])
+                            flag = True
+                hdf5_file.close()
+
+                # print("One file took %0.00f" % (time.time()-start_time))
+            #import pdb;pdb.set_trace()
+            targets_norm = (np.array(targets)-min_feat_tars)/(max_feat_tars-min_feat_tars)
+            inputs_norm = (np.array(inputs)-min_feat_ins)/(max_feat_ins-min_feat_ins)
+            #yield inputs, targets
+        yield inputs_norm, targets_norm
+            
 def get_stats():
     in_dir=config.dir_hdf5
     num_batches = config.batches_per_epoch_train
@@ -219,7 +164,7 @@ def get_stats():
     
 def main():
     # get_stats(feat='feats')
-    gen = data_gen()
+    gen = data_gen(data_aug = True)
     start_time = time.time()
     for inp, tar in gen:
         print(time.time()-start_time)